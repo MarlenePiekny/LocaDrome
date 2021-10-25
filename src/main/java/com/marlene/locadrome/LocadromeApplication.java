fc66483a344db354f4432ac35d3db70bd452835c
Marlene <marlene.piekny@le-campus-numerique.fr>
2021-10-25 13:24:12 +0200

    init: built project

---
+++ src/main/java/com/marlene/locadrome/LocadromeApplication.java
@@ -0,0 +1,13 @@
+package com.marlene.locadrome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocadromeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocadromeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marlene/locadrome/LocadromeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marlene.locadrome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocadromeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
