709e64b4f85130df36e9ea9c06a2998afcf4b3bc
Marlene <marlene.piekny@le-campus-numerique.fr>
2021-10-26 15:41:48 +0200

    feat: add swagger configuration

--- src/main/java/com/marlene/locadrome/LocadromeApplication.java
+++ src/main/java/com/marlene/locadrome/LocadromeApplication.java
@@ -2,8 +2,11 @@ package com.marlene.locadrome;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 
 @SpringBootApplication
+@EnableSwagger2
 public class LocadromeApplication {
 
 	public static void main(String[] args) {
